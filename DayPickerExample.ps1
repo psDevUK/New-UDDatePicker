e6e31ff7b200e916dffea8b2811b8595724cd453
Adam <adambacon1@hotmail.com>
2019-11-08 14:53:14 +0000

    Puts the date in nice easy sql format yyyy-MM-dd

--- DayPickerExample.ps1
+++ DayPickerExample.ps1
@@ -7,9 +7,13 @@ Start-UDDashboard -Port 10005 -Dashboard (
             New-UDColumn -size 6 -Content {
                 New-UDDatePicker -Id "Picker"
                 New-UDButton -Text "Get Dates" -OnClick {
-                    $from = (Get-UDElement -Id "Picker").Attributes.from
+                     $from = (Get-UDElement -Id "Picker").Attributes.from
+                    [datetime]$s = "$from"
+                    $Start = "$($s.Date.Year)-$($s.Date.Month)-$($s.Date.Day)"
                     $to = (Get-UDElement -Id "Picker").Attributes.to
-                    show-udtoast -message "You Selected $from until $to" -duration 5000 -Position center
+                    [datetime]$e = "$to"
+                    $End = "$($e.Date.Year)-$($e.Date.Month)-$($e.Date.Day)"
+                    show-udtoast -message "You Selected $Start until $End" -duration 5000 -Position center
 
 
                 }
